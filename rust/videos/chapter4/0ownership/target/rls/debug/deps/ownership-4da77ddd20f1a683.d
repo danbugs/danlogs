f627a38313569f0c73f70acd87b10ddb1b7a2733
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-04 23:41:58 -0800

    chapter 4.1 content

---
+++ rust/videos/chapter4/0ownership/target/rls/debug/deps/ownership-4da77ddd20f1a683.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/rust/youtube/rust/videos/chapter4/0ownership/target/rls/debug/deps/ownership-4da77ddd20f1a683.rmeta: src/main.rs
+
+/Users/carol/Documents/code/rust/youtube/rust/videos/chapter4/0ownership/target/rls/debug/deps/ownership-4da77ddd20f1a683.d: src/main.rs
+
+src/main.rs:
