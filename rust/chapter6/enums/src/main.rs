56053a2cef5426abe8b571d4c2c480c46f302aa7
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-11 18:02:09 -0800

    trying to fix makefile linguist issue

--- rust/chapter6/enums/src/main.rs
+++ rust/chapter6/enums/src/main.rs
@@ -1,21 +1,3 @@
-#[derive(Debug)]
-enum Message{
-    Quit,
-    Move { x: i32, y: i32},
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self){
-        println!("{:?}", self);
-    }
-}
-
 fn main() {
-    let m = Message::Write(String::from("ok!"));
-    m.call();
-    println!("{:?}", m);
-
-    let y : Option<i8> = Some(5);
+    println!("Hello, world!");
 }
