71c74adda1d341e7c845e3ceca0d87f75a86772f
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-15 11:05:00 -0800

    chapter 6 concluded

---
+++ rust/chapter7/restaurant/target/debug/restaurant-e5e00cd352c52c64.d
@@ -0,0 +1,1 @@
+/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/restaurant-e5e00cd352c52c64: /Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/src/lib.rs
