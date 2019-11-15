fdf083ebb8df6265d7670ab1de09593f696ed9be
danbugs <danilochiarlone@gmail.com>
2019-11-15 11:05:00 -0800

    chapter 6 concluded

---
+++ rust/chapter7/restaurant/target/debug/deps/restaurant-e5e00cd352c52c64.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/restaurant-e5e00cd352c52c64: src/lib.rs
+
+/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/restaurant-e5e00cd352c52c64.d: src/lib.rs
+
+src/lib.rs:
