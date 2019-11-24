0f327cc91d595634c90dc042cbcb2e2b0632c5aa
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-24 14:32:29 -0800

    chapter 7 part 1

--- rust/chapter7/restaurant/src/lib.rs
+++ rust/chapter7/restaurant/src/lib.rs
@@ -1,7 +1,43 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+mod front_of_house{
+    fn do_something(){}
+    pub mod hosting{
+        pub fn add_to_waitlist(){
+            crate::eat_at_restaurant();
+            //crate::front_of_house::do_something();
+            super::do_something();
+        }
+    }
+}
 
-        fn seat_at_table() {}
+fn eat_at_restaurant(){
+    // Absolute Paths
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative Paths
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    //meal.seasonal_fruint -> NOP!
+}
+
+mod back_of_house{
+    pub struct Breakfast{
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    pub enum Appetizer{
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast{
+        pub fn summer(toast: &str) -> Breakfast{
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
 }
--- rust/chapter7/restaurant/target/debug/deps/restaurant-7b1694436b9a3482.d
+++
@@ -1,7 +0,0 @@
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/restaurant-7b1694436b9a3482.rmeta: src/lib.rs
-
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/librestaurant-7b1694436b9a3482.rlib: src/lib.rs
-
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/restaurant-7b1694436b9a3482.d: src/lib.rs
-
-src/lib.rs:
