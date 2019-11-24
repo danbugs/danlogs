b57e3ab6631cb8092fad079340eeb230350451fa
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-24 14:32:29 -0800

    chapter 7 part 1

--- rust/chapter7/restaurant/target/debug/deps/restaurant-e5e00cd352c52c64.d
+++
@@ -1,5 +0,0 @@
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/restaurant-e5e00cd352c52c64: src/lib.rs
-
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/deps/restaurant-e5e00cd352c52c64.d: src/lib.rs
-
-src/lib.rs:
--- rust/chapter7/restaurant/target/debug/restaurant-e5e00cd352c52c64.d
+++
@@ -1,1 +0,0 @@
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/debug/restaurant-e5e00cd352c52c64: /Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/src/lib.rs
