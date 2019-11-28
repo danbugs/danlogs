00c2e53e8a3edebc96ef66887ce86fabea149b16
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-27 21:53:58 -0800

    concluded chapter 7

--- rust/chapter7/restaurant/target/rls/debug/deps/restaurant-9548dd61697d38a6.d
+++
@@ -1,5 +0,0 @@
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/rls/debug/deps/restaurant-9548dd61697d38a6.rmeta: src/lib.rs
-
-/Users/carol/Documents/code/rust/youtube/rust/chapter7/restaurant/target/rls/debug/deps/restaurant-9548dd61697d38a6.d: src/lib.rs
-
-src/lib.rs:
