2cd1d542d1a040317ab4a8ad16f222be2b5c9f44
Dan Chiarlone <danilochiarlone@gmail.com>
2019-12-23 12:16:45 -0300

    Chapter 8 - Vectors

---
+++ rust/chapter8/vectors/target/debug/deps/vectors-9f02faa7f546be3a.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/youtube/rust/chapter8/vectors/target/debug/deps/vectors-9f02faa7f546be3a: src/main.rs
+
+/Users/carol/Documents/code/youtube/rust/chapter8/vectors/target/debug/deps/vectors-9f02faa7f546be3a.d: src/main.rs
+
+src/main.rs:
---
+++ rust/chapter8/vectors/target/rls/debug/deps/vectors-239cfc6e3f67712d.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/youtube/rust/chapter8/vectors/target/rls/debug/deps/vectors-239cfc6e3f67712d.rmeta: src/main.rs
+
+/Users/carol/Documents/code/youtube/rust/chapter8/vectors/target/rls/debug/deps/vectors-239cfc6e3f67712d.d: src/main.rs
+
+src/main.rs:
