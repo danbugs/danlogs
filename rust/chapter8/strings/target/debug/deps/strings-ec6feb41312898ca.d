f0dbc600b484f7c5847c737dad407afae37e4a2b
Dan Chiarlone <danilochiarlone@gmail.com>
2019-12-24 16:53:39 -0300

    Chapter 8 - Strings

---
+++ rust/chapter8/strings/target/debug/deps/strings-ec6feb41312898ca.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/youtube/rust/chapter8/strings/target/debug/deps/strings-ec6feb41312898ca: src/main.rs
+
+/Users/carol/Documents/code/youtube/rust/chapter8/strings/target/debug/deps/strings-ec6feb41312898ca.d: src/main.rs
+
+src/main.rs:
---
+++ rust/chapter8/strings/target/debug/strings.d
@@ -0,0 +1,1 @@
+/Users/carol/Documents/code/youtube/rust/chapter8/strings/target/debug/strings: /Users/carol/Documents/code/youtube/rust/chapter8/strings/src/main.rs
