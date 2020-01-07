5965a1013938d1c287e2bb0263f6229f11cd276b
Dan Chiarlone <danilochiarlone@gmail.com>
2020-01-06 20:25:25 -0800

    Extra: quick derefs

---
+++ rust/chapter8/extra_derefs/target/debug/deps/extra_derefs-133e926527b17f7d.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/youtube/rust/chapter8/extra_derefs/target/debug/deps/extra_derefs-133e926527b17f7d: src/main.rs
+
+/Users/carol/Documents/code/youtube/rust/chapter8/extra_derefs/target/debug/deps/extra_derefs-133e926527b17f7d.d: src/main.rs
+
+src/main.rs:
