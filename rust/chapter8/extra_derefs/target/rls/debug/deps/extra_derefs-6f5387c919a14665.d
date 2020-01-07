8ff7a0dab25aad13d94fd2e771c342510b8eca35
danbugs <danilochiarlone@gmail.com>
2020-01-06 20:25:25 -0800

    Extra: quick derefs

---
+++ rust/chapter8/extra_derefs/target/rls/debug/deps/extra_derefs-6f5387c919a14665.d
@@ -0,0 +1,5 @@
+/Users/carol/Documents/code/youtube/rust/chapter8/extra_derefs/target/rls/debug/deps/extra_derefs-6f5387c919a14665.rmeta: src/main.rs
+
+/Users/carol/Documents/code/youtube/rust/chapter8/extra_derefs/target/rls/debug/deps/extra_derefs-6f5387c919a14665.d: src/main.rs
+
+src/main.rs:
