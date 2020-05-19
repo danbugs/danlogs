0a5e1ba8866518972f63d696f3fa9c8e026a641e
Dan Chiarlone <t-dachia@microsoft.com>
2020-05-18 19:23:26 -0700

    closures video

---
+++ rust/chapter13/closures/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let x = "ok".to_string();
+    let ok = || { // FnOnce
+       x + &"ie".to_string()
+    };
+    println!("{:?}", ok());
+    // println!("{:?}", ok()); ERROR!
+
+    let x1 = 1;
+    let ok1 = || { // Fn
+        x1 + 2
+    };
+    println!("{:?}", ok1());
+    println!("{:?}", x1);
+
+    let mut x2 = 1;
+    let mut ok2 = || { // FnMut
+        x2 += 1;
+        x2
+    };
+    println!("{:?}", ok2());
+    println!("{:?}", x2);
+
+    let mut x3 = vec![1, 2, 3];
+    let mut ok3 = move || {
+        x3.pop()
+    };
+    println!("{:?}", ok3());    
+
+}
