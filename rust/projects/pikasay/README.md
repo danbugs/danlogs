e8f6651262792c4f633931672a769a32c99b69a2
Dan Chiarlone <danilochiarlone@gmail.com>
2020-08-03 23:49:50 -0700

    pre-published pikasay project

---
+++ rust/projects/pikasay/README.md
@@ -0,0 +1,16 @@
+pikasay 0.1.0
+
+USAGE:
+    pikasay [FLAGS] [OPTIONS] [message]
+
+FLAGS:
+    -h, --help        Prints help information
+    -p, --pokeball    Pika...
+    -i, --stdin       Read the message from STDIN
+    -V, --version     Prints version information
+
+OPTIONS:
+    -f, --file <file>    Use your own ASCII Pikachu from a file
+
+ARGS:
+    <message>    Pika Pika? [default: Created by danlogs!]
---
+++ rust/projects/pikasay/tests/integration_tests.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+
+#[test]
+fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("pikasay")
+        .expect("binary exists")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Created by danlogs!"));
+    
+    Ok(())
+}
+
+#[test]
+fn fail_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>>{
+    Command::cargo_bin("pikasay")
+    .expect("binary exists")
+    .args(&["-f", "no/such/file.txt"])
+    .assert()
+    .failure();
+
+    Ok(())
+}
