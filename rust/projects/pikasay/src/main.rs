0dfca152d0160a101f2ec13d43a1177b7f07654e
danbugs <danilochiarlone@gmail.com>
2020-08-03 23:49:50 -0700

    pre-published pikasay project

---
+++ rust/projects/pikasay/src/main.rs
@@ -0,0 +1,100 @@
+use std::io::{self, Read};
+use structopt::StructOpt;
+use colored::*;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+
+const PIKACHU : &str = 
+"⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
+⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
+⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⢰⣹⡆⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿";
+
+const POKEBALL : &str = 
+"────────▄███████████▄────────
+─────▄███▓▓▓▓▓▓▓▓▓▓▓███▄─────
+────███▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███────
+───██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██───
+──██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██──
+─██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██─
+██▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓██
+██▓▓▓▓▓▓▓▓██░░░░░██▓▓▓▓▓▓▓▓██
+██▓▓▓▓▓▓▓██░░███░░██▓▓▓▓▓▓▓██
+███████████░░███░░███████████
+██░░░░░░░██░░███░░██░░░░░░░██
+██░░░░░░░░██░░░░░██░░░░░░░░██
+██░░░░░░░░░███████░░░░░░░░░██
+─██░░░░░░░░░░░░░░░░░░░░░░░██─
+──██░░░░░░░░░░░░░░░░░░░░░██──
+───██░░░░░░░░░░░░░░░░░░░██───
+────███░░░░░░░░░░░░░░░███────
+─────▀███░░░░░░░░░░░███▀─────
+────────▀███████████▀────────";
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(default_value = "Created by danlogs!")]
+    /// Pika Pika?
+    message: String,
+
+    #[structopt(short = "p", long = "pokeball")]
+    /// Pika...
+    pokeball: bool,
+
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Use your own ASCII Pikachu from a file.
+    file: Option<std::path::PathBuf>,
+
+    #[structopt(short = "i", long = "stdin")]
+    /// Read the message from STDIN
+    stdin: bool,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let options = Options::from_args();
+    let mut message = "".to_string();
+    if options.stdin {
+        io::stdin().read_to_string(&mut message)?;
+        message.pop();
+    } else {
+        message = options.message;
+    }
+    if options.pokeball {
+        println!("{}", POKEBALL.to_string().red());
+    }
+    else {
+        match &options.file {
+            Some(path) => {
+                let alt = std::fs::read_to_string(path)
+                    .with_context(|_| format!("could not read file {:?}", path))?;
+                print_message(message);
+                println!("{}", alt.bright_yellow());
+            }
+            None => { 
+                print_message(message);
+                println!("{}", PIKACHU.to_string().bright_yellow())
+            }
+        }
+    }
+    Ok(())
+}
+
+fn print_message(message : String) {
+    if message.to_lowercase().contains("raichu") {
+        eprintln!("Pikachu hasn't evolved yet...");
+    } else {
+        println!(" {:-<1$}", "", message.chars().count() + 2);
+        println!("| {} |", message);
+        println!(" {:-<1$}", "", message.chars().count() + 2);
+        println!("  \\  /");
+        println!("   \\/");
+    }
+}
